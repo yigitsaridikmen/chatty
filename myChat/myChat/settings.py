86c89abd249200ede84c24edd22344954967939d
yigidos <saridikmenyigit@gmail.com>
2023-12-11 21:42:55 +0300

    ASGI set, daphne and channels installed and chatapp created

--- myChat/myChat/settings.py
+++ myChat/myChat/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chatapp.apps.ChatappConfig',
+    'channels',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myChat.wsgi.application'
-
+ASGI_APPLICATION = 'myChat.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases

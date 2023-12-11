5ea9abfb8fd9e736d7c55db6f6e6d8bcebd8fe8a
yigidos <saridikmenyigit@gmail.com>
2023-12-11 22:27:21 +0300

    Async single chat has been created

--- myChat/myChat/asgi.py
+++ myChat/myChat/asgi.py
@@ -15,3 +15,18 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myChat.settings')
 
 application = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter , URLRouter
+from chatapp import routing
+
+application = ProtocolTypeRouter(
+	{
+		"http" : get_asgi_application() , 
+		"websocket" : AuthMiddlewareStack(
+			URLRouter(
+				routing.websocket_urlpatterns
+			) 
+		)
+	}
+)
